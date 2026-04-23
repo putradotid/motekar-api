bedd6ec1739dcb15f53deb4ffe1a2405d6767e3a
putradotid <aprildwi999@gmail.com>
2026-04-23 16:06:28 +0700

    Membuat api untuk meeting request dan meeting log untuk user (meeting request belum dicoba)

--- app/Http/Controllers/Api/MeetingController.php
+++ app/Http/Controllers/Api/MeetingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\MeetingRequests;
 use Illuminate\Http\Request;
 
 class MeetingController extends Controller
@@ -13,5 +14,30 @@ class MeetingController extends Controller
             'title' => 'required|string|max:255',
             'date' => 'required|date'
         ]);
+
+        $user = $request->attributes->get('user');
+
+        $meeting = MeetingRequests::created([
+            'user_id' => $user->id,
+            'title' => $request->title,
+            'description' => $request->description,
+            'date' => $request->date,
+        ]);
+
+        return response()->json([
+            'message' => 'Meeting dibuat',
+            'data' => $meeting
+        ], 201);
     }
+
+    // user melihat list milih sendiri
+    public function MyMeetings(Request $request) {
+        $user = $request->attributes->get('user');
+
+        $data = MeetingRequests::where('user_id', $user->id, )->latest()->get();
+
+        return response()->json($data);
+    }
+    
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\MeetingController;
 
 // Auth
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
 Route::middleware('auth.token')->group(function() {
+    
+    // route user
+    Route::post('/meetings', [MeetingController::class, 'store']);
+    Route::get('/my-meetings', [MeetingController::class, 'MyMeetings']);
+    
+    // route admin
+    Route::post('/admin/meetings', [MeetingController::class, 'index']);
+    Route::put('/admin/meetings/{id}/approve', [MeetingController::class, 'approve']);
+    Route::put('/admin/meetings/{id}/reject', [MeetingController::class, 'reject']);
+
+    // logout
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
+// testing api dan middleware (optional bisa di hapus)
 Route::get('/test', function () {
     return response()->json(['message' => 'API OK']);
 });
